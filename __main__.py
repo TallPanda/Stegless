760a131cf2a0e203ea3f8b06ad76088f7ef4a03f
TallPanda <b00102169@mytudublin.ie>
2021-03-15 13:19:41 +0000

    Strings and Readme
    Updated readme with usage
    Fixed issue with Strings and allowed user input

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
 from stegless.magicnum import getmagic
 from stegless.strings import strings
-def main():
-    print (getmagic(__file__))
-    print (strings("strings-stego.jpg","flag{","}"))
+from sys import argv
+def main(_file):
+    print (getmagic(_file))
+    print (strings(_file,"flag{","}"))
 
 if __name__ == "__main__":
-    main()
+    main(argv[1])
