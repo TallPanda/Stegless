e395ce08856ba88f2e4469a850c36243c64cb5b4
TallPanda <b00102169@mytudublin.ie>
2021-03-15 13:02:51 +0000

    Main update

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from stegless.magicnum import getmagic
+from stegless.strings import strings
 def main():
     print (getmagic(__file__))
+    print (strings("strings-stego.jpg","f","}"))
 
 if __name__ == "__main__":
     main()
