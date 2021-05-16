247856fbd981f1b74fdea5aeac8103d2f4d2b518
TallPanda <b00102169@mytudublin.ie>
2021-05-16 18:09:32 +0100

    Stegtools output added

--- stegless/steg_tools.py
+++ stegless/steg_tools.py
@@ -1,17 +1,16 @@
 from subprocess import run
-from sys import argv
 
 def steghide(file: str, password: str=''):
     assert(isinstance(file,str)),f"Path(AKA file) not a string\nfile: {file}\nType: {type(file)}"
     assert(isinstance(password,str)),f"Path(AKA password) not a string\npassword: {password}\nType: {type(password)}"
     if file[0] =='-':
         raise(Exception("Cannot start file name with '-'"))
-    run(['steghide', 'extract', '-sf', file, '-p', password])
+    return(str(run(['steghide', 'extract', '-sf', file, '-p', password], capture_output=True).stdout))
     
 def openstego(file: str, password: str=''):
     assert(isinstance(file,str)),f"Path(AKA file) not a string\nfile: {file}\nType: {type(file)}"
     assert(isinstance(password,str)),f"Path(AKA password) not a string\npassword: {password}\nType: {type(password)}"
     if password != '':
-        run(['openstego', 'extract', '-sf', file, '-p', password])
+        return(str(run(['openstego', 'extract', '-sf', file, '-p', password], capture_output=True).stdout))
     else:
-        run(['openstego', 'extract', '-sf', file])
+        return(str(run(['openstego', 'extract', '-sf', file], capture_output=True).stdout))
